83742a394c48cd4f5c618b072cdd3d2c34d1a322
Artem Kolesnikov <artemiyk2017@gmail.com>
2026-03-27 22:43:40 +0100

    fix: requested changes

--- app/main.py
+++ app/main.py
@@ -8,22 +8,18 @@ class Person:
 
 
 def create_person_list(people_data: list[dict]) -> list[Person]:
-    result_list = []
+    Person.people.clear()
 
     for person in people_data:
-        Person.people[person.get("name")] = Person(
-            person.get("name"),
-            person.get("age")
-        )
+        Person(person.get("name"), person.get("age"))
 
     for person in people_data:
         name = person.get("name")
-        if person.get("wife") and person.get("wife") is not None:
+        if person.get("wife"):
             spouse_name = person.get("wife")
             Person.people[name].wife = Person.people[spouse_name]
-        elif person.get("husband") and person.get("husband") is not None:
+        elif person.get("husband"):
             spouse_name = person.get("husband")
             Person.people[name].husband = Person.people[spouse_name]
-        result_list.append(Person.people[name])
 
-    return result_list
+    return [Person.people[p.get("name")] for p in people_data]
